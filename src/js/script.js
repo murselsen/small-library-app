a492c8d555927457191900280c43873e3a9542ca
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-25 22:27:32 +0300

    ``` Refactor script.js to enhance addBook function and optimize book list management ```

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,8 @@ const bookList = document.querySelector("#bookList");
 
 let bookListArray = JSON.parse(localStorage.getItem("bookListArray") || "[]");
 console.log(bookListArray);
-const addBook = (book) => {
+
+const addBookData = (book) => {
   let result = [...bookListArray, book];
   localStorage.setItem("booklistArray", result);
 };
@@ -20,31 +21,3 @@ addBookButton.addEventListener("click", () => {
   bookName.value = "";
   bookAuthor.value = "";
 });
-
-const bookName = document.querySelector("#bookName");
-const authorName = document.querySelector("#authorName");
-const addButton = document.querySelector("#addBook");
-const bookList = document.getElementById("bookList");
-
-function addBook(event) {
-  const name = bookName.value.trim();
-  const author = authorName.value.trim();
-  if (name === "" || author === "") {
-    alert("Lütfen kitap adı veya yazar adı giriniz.");
-  }
-
-  const li = document.createElement("li");
-  li.textContent = `${name} - ${author}`;
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "sil";
-
-  li.appendChild(deleteBtn);
-  bookList.appendChild(li);
-
-  deleteBtn.addEventListener("click", (e) => {
-    li.remove();
-  });
-}
-
-addButton.addEventListener("click", addBook);
