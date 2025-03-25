20f47396f1c06355bfe8cfca4acbb3c135a35a27
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-25 21:04:55 +0300

    Localize index.html to Turkish, enhance layout for Mini Kütüphane, add styles in styles.css, and implement basic functionality in script.js

---
+++ src/js/script.js
@@ -0,0 +1,7 @@
+const addBookButton = document.querySelector("#addBook");
+
+
+const bookName = document.querySelector("#bookName");
+const bookAuthor = document.querySelector("#authorName");
+
+const bookList = document.querySelector("#bookList");
