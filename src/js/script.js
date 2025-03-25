550983fcb791f4aaee1e3224d0fbc2351ef71617
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-25 22:01:09 +0300

    Update index.html to set script type to module for script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,33 @@
 const addBookButton = document.querySelector("#addBook");
-
-
 const bookName = document.querySelector("#bookName");
 const bookAuthor = document.querySelector("#authorName");
-
 const bookList = document.querySelector("#bookList");
+
+
+
+
+let bookListArray = JSON.parse(
+    localStorage.getItem("bookListArray") || "[]"
+);
+
+const addBook = (book) => {
+    
+};
+
+
+addBookButton.addEventListener("click", () => {
+  const valueBookName = bookName.value;
+  const valueAuthorName = bookAuthor.value;
+
+  let book = {
+    name: valueBookName,
+    author: valueAuthorName,
+  };
+
+  bookListArray.push(book);
+
+  bookName.value = "";
+  bookAuthor.value = "";
+});
+
+
